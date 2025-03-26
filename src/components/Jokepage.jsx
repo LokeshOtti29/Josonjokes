b7d03ffba1ee178161ed91064295bc05db5784f2
LokeshOtti29 <lokeshotti456@gmail.com>
2025-03-26 13:04:09 +0530

    rrd

--- src/components/Jokepage.jsx
+++ src/components/Jokepage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Center from './Center'
+import Type from './Type'
+import Pagination from './Pagination'
 
 const Jokepage = () => {
   return (
     <div>
-      
+      <Center/>
+      <Type/>
+      <div>
+        <Pagination/>
+      </div>
     </div>
   )
 }
