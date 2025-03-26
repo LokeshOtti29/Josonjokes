3a84fd2e2aba68448c2ec4ec0ac3c04e33d7c0c4
LokeshOtti29 <lokeshotti456@gmail.com>
2025-03-26 12:48:09 +0530

    Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <h1>JSON Jokes</h1>
+        </div>
+        <div>
+            <button>Take Me to the Funny Side &#x1F606;</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
