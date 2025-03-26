bc226bb28a5ef93e1418eb71ed3081afe701b662
LokeshOtti29 <lokeshotti456@gmail.com>
2025-03-26 12:57:17 +0530

    change2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='d-inline-flex'>
         <div>
-            <h1>JSON Jokes</h1>
+            <h1 className='caveat-head'>JSON Jokes</h1>
         </div>
         <div>
             <button>Take Me to the Funny Side &#x1F606;</button>
